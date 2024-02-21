8e3b6a24fae682b2608c816e4573566a511d0b9e
Ellyzeul <gabrielaug.240@gmail.com>
2024-02-21 03:05:09 -0300

    refactor: pipeline de configuração dos exemples reestruturado

--- examples/index.ts
+++ examples/index.ts
@@ -5,18 +5,11 @@ import { spawn } from "child_process";
 const example = argv[2] as string
 
 fs.readdir('examples')
-  .then(files => files
-    .filter(file => file.endsWith('.ts') && !file.startsWith('index'))
-    .map(file => file.replace('.ts', ''))
-  )
-  .then(examples => {
-    if(!examples.includes(example)) {
-      console.log(`Choose an example from the list:\n${examples.map(file => `\n   - ${file}`).join('')}\n`)
-      exit(1)
-    }
-    
-    spawn('node', [`${example}.js`], {
-      stdio: 'inherit',
-      cwd: 'dist/examples'
-    })
+  .then(files => files.filter(file => file.endsWith('.ts') && !file.startsWith('index')))
+  .then(files => files.map(file => file.replace('.ts', '')))
+  .then(examples => { if(!examples.includes(example)) throw examples })
+  .then(() => spawn('node', [`${example}.js`], { stdio: 'inherit', cwd: 'dist/examples' }))
+  .catch((examples: Array<string>) => {
+    console.log(`Choose an example from the list:\n${examples.map(file => `\n   - ${file}`).join('')}\n`)
+    exit(1)
   })
